b05c4e4f3816fb8dd79d4f76e748b24b636b91b1
Keith <keithpacana@berkeley.edu>
2019-08-04 20:12:01 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Typeahead} from 'react-bootstrap-typeahead';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: [],
+      suggestions: []
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  /* On input change make API call with the input search query of length > 3*/
+  handleInputChange(e) {
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=af8045b&s=${e.valueOf()}`).then(function(res) {
+        if (res.ok) {
+          return res.json();
+        }
+    }).then(function(json)
+      {
+        if (json.Response !== "False" && e.valueOf().length > 3) {
+          let s = [];
+          var i;
+          for (i = 0; json.Search.length > i; i++) {
+            s.push({
+                  id:json.Search[i].Title,
+                  text: json.Search[i].Title,
+                  year: json.Search[i].Year
+                  });
+          }
+          if (s.length > 0) {this.setState({suggestions: s})};
+        }
+      }.bind(this));
+  }
+
+  render() {
+    return (
+        <div className="container">
+          <div className="text-container">
+            <span>Search for a movie</span>
+          </div>
+          <Typeahead
+              clearButton
+              id="main"
+              labelKey="id"
+              paginate={true}
+              maxResults={10}
+              renderMenuItemChildren={(option) => (
+                  <div>
+                    <div>{option.id}</div>
+                    <small> {option.year}</small>
+                  </div>
+              )}
+              multiple
+              options={this.state.suggestions}
+              onInputChange={(e) => this.handleInputChange(e)}
+              placeholder="Search by title..."
+          />
+        </div>
+    )
+  }
+};
 export default App;
